59582a7bba16a007b5d1b4b4bf3429f2ecb4cb1f
Shad-islam <shadislam48@gmail.com>
2023-09-07 01:52:45 +0600

    Add Upper and Lower button with fuctionality

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 const input = document.querySelector("#input");
-const countBtn = document.querySelector(".container__details-btns");
 const count = document.querySelector(".container__details-title span");
+const countBtn = document.querySelector(".container__details-countBtn");
+const upperCaseBtn = document.querySelector(".container__details-upperCase");
+const lowerCaseBtn = document.querySelector(".container__details-lowerCase");
 
+// word count fuction
 function wordCounter(input) {
   let inputValue = input.value ? input.value : 0;
   let wordCount = inputValue.split(/\s+/).length;
   count.textContent = wordCount;
 }
-
 countBtn.addEventListener("click", function () {
   wordCounter(input);
+});
+// upper case fuction
+function wordUpperCase(input) {
+  input.value = input.value.toUpperCase();
+}
+upperCaseBtn.addEventListener("click", function () {
+  wordUpperCase(input);
+});
+// lower case fuction
+function wordLowerCase(input) {
+  input.value = input.value.toLowerCase();
+}
+lowerCaseBtn.addEventListener("click", () => {
+  wordLowerCase(input);
 });
